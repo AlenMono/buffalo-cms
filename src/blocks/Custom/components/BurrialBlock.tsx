c6aeac9bb407324c0f39f0f45fcee4998d471185
Alen <arajher@mono.software>
2025-12-09 11:12:08 +0100

    planning icon

--- src/blocks/Custom/components/BurrialBlock.tsx
+++ src/blocks/Custom/components/BurrialBlock.tsx
@@ -38,12 +38,13 @@ interface BurialBlockProps {
 const BurialBlock: React.FC<BurialBlockProps> = ({ sectionTitle, image, burialOptions, burialLayout, burialDescription }) => {
     if (burialLayout === 'horizontal') {
         return (
-            <div className="p-3 pl-5 bg-secondary border border-primary-dark rounded-lg">
+            <div className="p-3 pl-5 bg-secondary border border-primary-darkest rounded-lg">
                 <div className='flex flex-col justify-between gap-5'>
-                    <div className="flex justify-between items-end flex-1 mb-28">
-                        {sectionTitle && <RichText data={sectionTitle} className='text-left !text-5xl font-faustina mb-14 md:mb-0' />}
+                    <div className="flex flex-col md:flex-row justify-between md:items-end gap-4 flex-1 mb-4 md:mb-28">
+                        {sectionTitle && <RichText data={sectionTitle} paragraphClassName="!text-2xl lg:!text-4xl leading-[32px] lg:!leading-[44px] xl:!text-[44px] xl:leading-[52px]"
+                            h1ClassName="!text-3xl lg:!text-4xl leading-[32px] lg:!leading-[44px] xl:!text-[44px] xl:leading-[52px]" />}
 
-                        <div className='max-w-[490px] text-base'>
+                        <div className='max-w-[490px] text-xs md:text-base'>
                             {burialDescription}
                         </div>
                     </div>
@@ -51,7 +52,7 @@ const BurialBlock: React.FC<BurialBlockProps> = ({ sectionTitle, image, burialOp
                     <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4 flex-1">
                         {
                             burialOptions?.map((option, index) => {
-                                return <div key={index} className='bg-background border border-primary-dark rounded-lg p-5'>
+                                return <div key={index} className='bg-[#FFF9EE] border border-primary-darkest rounded-lg p-4 md:p-5'>
                                     <div className='flex flex-col justify-between items-start h-full gap-7'>
                                         <Image
                                             width={263}
@@ -61,8 +62,8 @@ const BurialBlock: React.FC<BurialBlockProps> = ({ sectionTitle, image, burialOp
                                             className='object-cover w-full rounded-lg'
                                         />
                                         <div className='flex flex-col items-start gap-3'>
-                                            <h4 className="text-2xl font-semibold">{option.title}</h4>
-                                            {option.burialBadges?.map((badge, index) => <p key={index} className='text-sm border rounded-full px-3 py-1 text-brand-30'>{badge.title}</p>)}
+                                            <h4 className="text-lg md:text-2xl font-semibold">{option.title}</h4>
+                                            {option.burialBadges?.map((badge, index) => <p key={index} className='text-xs md:text-sm border border-brand-30 rounded-full px-2 md:px-3 py-1 text-brand-30'>{badge.title}</p>)}
                                         </div>
                                     </div>
                                 </div>
@@ -87,7 +88,7 @@ const BurialBlock: React.FC<BurialBlockProps> = ({ sectionTitle, image, burialOp
                 </div>
             </div>
             <div className="hidden md:flex">
-                <div className='p-3 rounded-lg relative bg-background-light border border-gold'>
+                <div className='p-3 rounded-lg relative bg-background-light border border-primary-darkest'>
                     <Image width={760} height={660} src={image?.url || 'public/img/graveyard.png'} alt="Cemetery" className='h-full object-cover' />
                 </div>
             </div>
--- src/blocks/Custom/components/PlanningBlock.tsx
+++ src/blocks/Custom/components/PlanningBlock.tsx
@@ -2,121 +2,145 @@ import RichText from '@/components/RichText'
 import { Button, buttonVariants } from '@/components/ui/button'
 import { cn } from '@/utilities/ui'
 import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import PreNeedIcon from '/public/img/planning/pre-need.svg'
+import AtNeedIcon from '/public/img/planning/at-need.svg'
+import NearNeedIcon from '/public/img/planning/near-need.svg'
 
 type PlanningCard = {
-  heading: string
-  subheading: string
-  description: string
-  link?: string // button label or text
-  buttonLabel?: string // ✅ new field for button target (URL or route)
-  badges?: { badge: string }[] // ✅ new field for badge list
-  cardBackground?: 'default' | 'floral'
-  buttonVariant: 'default' | 'outline' | 'primary' | 'primary-outline'
+    heading: string
+    subheading: string
+    description: string
+    link?: string // button label or text
+    buttonLabel?: string // ✅ new field for button target (URL or route)
+    badges?: { badge: string }[] // ✅ new field for badge list
+    cardBackground?: 'default' | 'floral'
+    buttonVariant: 'default' | 'outline' | 'primary' | 'primary-outline'
 }
 
 type PlanningBlockProps = {
-  planningCards: PlanningCard[]
-  planningTitle?: any
-  description?: string
+    planningCards: PlanningCard[]
+    planningTitle?: any
+    description?: string
 }
 
 export const PlanningBlock: React.FC<PlanningBlockProps> = ({
-  planningTitle,
-  description,
-  planningCards,
+    planningTitle,
+    description,
+    planningCards,
 }) => {
-  return (
-    <section>
-      <div
-        className={cn(
-          'mx-auto space-y-8',
-          planningCards[0]?.badges && planningCards[0]?.badges?.length > 0
-            ? 'max-w-[1320px]'
-            : 'max-w-[1128px]',
-        )}
-      >
-        <div className="grid grid-cols-1 md:grid-cols-2 mb-10 gap-5">
-          {planningTitle && (
-            <RichText data={planningTitle} paragraphClassName="text-left !text-4xl" />
-          )}
-          {description && (
-            <div className="flex md:justify-end">
-              <p className="max-w-[460px] text-lg text-brand-30">{description}</p>
-            </div>
-          )}
-        </div>
-
-        <div className="flex -mx-4 px-4 overflow-auto xl:overflow-visible xl:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:items-center gap-6">
-          {planningCards.map((card, idx) => (
-            <Link
-              href={card.link || ''}
-              key={idx}
-              className={cn(
-                'card-hovered flex py-4 px-6 pb-5 rounded-lg relative min-h-[188px] min-w-[277px] border border-primary-dark cursor-pointer',
-                card.cardBackground === 'floral' ? 'bg-floral-pattern' : 'bg-white',
-                idx % 2 !== 0 && 'bg-floral-pattern-2',
-              )}
-            >
-              <div className="relative flex flex-col z-10">
-                <div className="flex-1">
-                  <h3 className="text-[28px] leading-[36px] font-semibold mb-1 font-faustina italic">
-                    {card.heading}
-                  </h3>
-                  <p className="text-sm text-brand-30 mb-4">{card.subheading}</p>
-
-                  {/* ✅ New: Badges list */}
-                  {card.badges && card.badges.length > 0 && (
-                    <div className="flex flex-wrap gap-1 gap-y-2 mb-4 mt-44">
-                      {card.badges.map((b, i) => (
-                        <span
-                          key={i}
-                          className="text-sm border rounded-full px-3 py-1 text-brand-30"
-                        >
-                          {b.badge}
-                        </span>
-                      ))}
-                    </div>
-                  )}
-                </div>
-
-                <div className="flex justify-between items-center gap-4">
-                  <p
-                    className={cn(
-                      'text-brand-30',
-                      card.badges && card.badges.length > 0 ? 'text-lg' : 'text-base',
-                    )}
-                  >
-                    {card.description}
-                  </p>
-
-                  {card.link && !card.buttonLabel && (
-                    <Button
-                      className={buttonVariants({
-                        variant: 'outline',
-                        className: 'min-w-10 min-h-10 !p-0',
-                      })}
-                    >
-                      <ArrowRight className="h-6 w-6" />
-                    </Button>
-                  )}
-                </div>
-                {card.buttonLabel && (
-                  <Button
-                    className={buttonVariants({
-                      variant: card.buttonVariant,
-                      className: 'mt-6',
-                    })}
-                  >
-                    {card.buttonLabel}
-                  </Button>
+    return (
+        <section>
+            <div
+                className={cn(
+                    'mx-auto space-y-8',
+                    planningCards[0]?.badges && planningCards[0]?.badges?.length > 0
+                        ? 'max-w-[1320px]'
+                        : 'max-w-[1128px]',
                 )}
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </section>
-  )
+            >
+                <div className="grid grid-cols-1 md:grid-cols-2 mb-10 gap-5">
+                    {planningTitle && (
+                        <RichText data={planningTitle} paragraphClassName="text-left !text-4xl" />
+                    )}
+                    {description && (
+                        <div className="flex md:justify-end">
+                            <p className="max-w-[460px] text-lg text-brand-30">{description}</p>
+                        </div>
+                    )}
+                </div>
+
+                <div className="flex -mx-4 px-4 overflow-auto lg:overflow-visible lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:items-center gap-6">
+                    {planningCards.map((card, idx) => {
+                        let backgroundIcon = PreNeedIcon;
+
+                        switch (idx) {
+                            case 0:
+                                backgroundIcon = PreNeedIcon;
+                                break;
+                            case 1:
+                                backgroundIcon = AtNeedIcon;
+                                break;
+                            case 2:
+                                backgroundIcon = NearNeedIcon;
+                                break;
+                        }
+
+                        const isCardWithBadges = card.badges && card.badges.length > 0;
+
+                        return (
+                            <Link
+                                href={card.link || ''}
+                                key={idx}
+                                className='card-hovered flex py-4 pt-3 px-4 lg:px-6 pb-5 rounded-lg relative min-h-[188px] min-w-[300px] border border-primary-dark cursor-pointer bg-background-light overflow-hidden'
+                            >
+                                <div className="flex flex-col z-10">
+                                    <div className="flex-1">
+                                        <h3 className={cn("font-semibold mb-1 font-faustina italic", isCardWithBadges ? "text-[24px] leading-[32px] lg:text-4xl" : "text-xl font-semibold mb-1")}>
+                                            {card.heading}
+                                        </h3>
+                                        <p className={cn("text-brand-30 mb-4", isCardWithBadges ? 'text-xs lg:text-base' : 'text-xs md:text-sm')}>{card.subheading}</p>
+
+                                        {isCardWithBadges && (
+                                            <div className="flex flex-wrap gap-1 gap-y-2 mb-4 mt-44">
+                                                {card.badges?.map((b, i) => (
+                                                    <span
+                                                        key={i}
+                                                        className="text-sm border rounded-full px-3 py-1 text-brand-30"
+                                                    >
+                                                        {b.badge}
+                                                    </span>
+                                                ))}
+                                            </div>
+                                        )}
+                                    </div>
+
+                                    <div className="flex justify-between items-center gap-4">
+                                        <p
+                                            className={cn(
+                                                'text-brand-30',
+                                                isCardWithBadges ? 'text-sm lg:text-lg' : 'text-sm lg:text-base',
+                                            )}
+                                        >
+                                            {card.description}
+                                        </p>
+
+                                        {card.link && !card.buttonLabel && (
+                                            <Button
+                                                className={buttonVariants({
+                                                    variant: 'outline',
+                                                    className: 'min-w-10 min-h-10 !p-0',
+                                                })}
+                                            >
+                                                <ArrowRight className="h-6 w-6" />
+                                            </Button>
+                                        )}
+                                    </div>
+                                    {card.buttonLabel && (
+                                        <Button
+                                            className={buttonVariants({
+                                                variant: card.buttonVariant,
+                                                className: 'mt-6',
+                                            })}
+                                        >
+                                            {card.buttonLabel}
+                                        </Button>
+                                    )}
+                                </div>
+                                <Image
+                                    src={backgroundIcon}
+                                    alt={card.heading}
+                                    className={cn('absolute right-0 z-0', idx === 0 ? "top-0" : "top-6")}
+                                    width={170}
+                                    height={420}
+                                />
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </section >
+    )
 }
