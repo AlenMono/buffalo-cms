f62bbae2c001096c8848ec781e39a5803252b340
Alen <arajher@mono.software>
2026-01-14 08:39:03 +0100

    added empty state

--- src/blocks/Custom/components/PlanningProccessBlock.tsx
+++ src/blocks/Custom/components/PlanningProccessBlock.tsx
@@ -25,14 +25,30 @@ export const PlanningProcessBlock: React.FC<PlanningProcessBlockProps> = ({
     planningProcessCards = [],
     planningProcessLayout,
 }) => {
-    let listClassName = ""
+    let listClassName,
+        cardSpacing = ''
 
-    if (planningProcessLayout === "steps-a") {
-        listClassName = "grid-cols-1 md:grid-cols-2 xl:grid-cols-4"
+    if (planningProcessLayout === 'steps-a') {
+        listClassName = 'grid-cols-1 md:grid-cols-2 xl:grid-cols-4'
+        cardSpacing = 'gap-8 xl:gap-[132px]'
     }
-    if (planningProcessLayout === "steps-c") {
-        listClassName = "grid-cols-1 md:grid-cols-2"
+    if (planningProcessLayout === 'steps-c') {
+        listClassName = 'grid-cols-1 md:grid-cols-2'
+        cardSpacing = 'gap-8 gap-12'
+
+        if (planningProcessCards.length === 0) {
+            return (
+                <div className="flex flex-col justify-center items-center gap-3 bg-background-light border border-primary-dark p-5 lg:px-9 lg:py-7 rounded-lg relative overflow-hidden h-[416px] text-center">
+                    <h3 className="section-title font-faustina text-brand-10">No Current <br /> Openings</h3>
+                    <p className="text-base text-brand-30 max-w-[493px] mx-auto">
+                        We don’t have any open positions at the moment. Please check back in the future as
+                        opportunities may become available.
+                    </p>
+                </div>
+            )
+        }
     }
+
     return (
         <section className="max-w-[1320px] mx-auto">
             <div className="bg-background-light border border-primary-dark p-5 lg:px-9 lg:py-7 rounded-lg relative overflow-hidden">
@@ -51,10 +67,11 @@ export const PlanningProcessBlock: React.FC<PlanningProcessBlockProps> = ({
                             height={958}
                             alt="Wreath"
                             className="absolute -top-20 h-[290px] md:h-auto md:top-1/2 -right-[55%] transform md:translate-y-[-50%] scale-x-[-1] "
-                        /></>
+                        />
+                    </>
                 )}
 
-                <div className='z-10 relative'>
+                <div className="z-10 relative">
                     {planningProcessTitle && (
                         <div className="mb-6  text-center">
                             <RichText data={planningProcessTitle} className="section-title" />
@@ -68,30 +85,39 @@ export const PlanningProcessBlock: React.FC<PlanningProcessBlockProps> = ({
                     )}
 
                     {planningProcessCards.length > 0 && (
-                        <div className={cn("grid gap-4 flex-1", listClassName)}>
+                        <div className={cn('grid gap-4 flex-1', listClassName)}>
                             {planningProcessCards.map((card, idx) => (
                                 <div
                                     key={idx}
-                                    className="p-5 bg-background border border-primary-dark rounded-lg flex flex-col justify-between gap-8 xl:gap-[132px]"
+                                    className={cn(
+                                        'p-5 bg-background border border-primary-dark rounded-lg flex flex-col justify-between',
+                                        cardSpacing,
+                                    )}
                                 >
-                                    <p className="font-faustina font-semibold text-lg md:text-2xl text-gold">0{idx + 1}</p>
+                                    <p className="font-faustina font-semibold text-lg md:text-2xl text-gold">
+                                        0{idx + 1}
+                                    </p>
 
                                     <div className="flex flex-col gap-3">
                                         <h3 className="text-lg md:text-2xl font-bold text-brand">{card.heading}</h3>
                                         <p className="text-brand-30 text-sm">{card.description}</p>
-                                        {card.link &&
-                                            <div className='flex items-center gap-2'>
-                                                <Link href={card.link || ''} className="text-sm font-bold items-center relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-300 hover:after:w-full">
+                                        {card.link && (
+                                            <div className="flex items-center gap-2">
+                                                <Link
+                                                    href={card.link || ''}
+                                                    className="text-sm font-bold items-center relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-300 hover:after:w-full"
+                                                >
                                                     {card.linkLabel || 'View all options'}
                                                 </Link>
                                                 <MoveRightIcon />
                                             </div>
-                                        }
+                                        )}
                                     </div>
                                 </div>
                             ))}
                         </div>
-                    )}</div>
+                    )}
+                </div>
             </div>
         </section>
     )
